2ec7d3e22b432208724c7d129bf21a966e69713f
lugasnugroho <lugasnugroho957@gmail.com>
2026-09-09 01:50:09 +0700

    Add GameList.lua with game URLs

---
+++ GameList.lua
@@ -0,0 +1,4 @@
+return {
+    [123456789] = "https://raw.githubusercontent.com/lugasnugroho/ThorHub/main/Games/GameA.lua",
+    [987654321] = "https://raw.githubusercontent.com/lugasnugroho/ThorHub/main/Games/GameB.lua"
+}
