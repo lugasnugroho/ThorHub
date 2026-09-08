9180f3c3f6365db95926fdfbb7d4b6bbe21edb80
lugasnugroho <lugasnugroho957@gmail.com>
2026-09-09 02:11:44 +0700

    Refactor GameList to include game names and URLs

--- GameList.lua
+++ GameList.lua
@@ -1,4 +1,13 @@
 return {
-    [123456789] = "https://raw.githubusercontent.com/lugasnugroho/ThorHub/main/Games/GameA.lua",
-    [987654321] = "https://raw.githubusercontent.com/lugasnugroho/ThorHub/main/Games/GameB.lua"
+
+    [123456789] = {
+        Name = "Game A",
+        URL = "https://raw.githubusercontent.com/lugasnugroho/ThorHub/main/Games/GameA.lua"
+    },
+
+    [987654321] = {
+        Name = "Game B",
+        URL = "https://raw.githubusercontent.com/lugasnugroho/ThorHub/main/Games/GameB.lua"
+    }
+
 }
